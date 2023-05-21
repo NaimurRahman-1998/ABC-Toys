a0fba70c23bbae56c5dca8d60ddf62a930498abb
NaimurRahman-1998 <nrturzo1998111@gmail.com>
2023-05-21 16:15:35 +0600

    submitted

--- src/pages/home/Partners.jsx
+++ src/pages/home/Partners.jsx
@@ -3,7 +3,7 @@ import img2 from '../../assets/Partners/shopno.jpg'
 import img3 from '../../assets/Partners/kids.jpg'
 import img4 from '../../assets/Partners/well.jpg'
 import img5 from '../../assets/Partners/lavender.jpg'
-
+import img6 from '../../assets/Partners/extra-Section.jpg'
 const Partners = () => {
     return (
         <div className="mt-16">
@@ -15,6 +15,11 @@ const Partners = () => {
                 <img className='w-[9rem]' src={img3} alt="" />
                 <img className='w-[9rem]' src={img4} alt="" />
                 <img className='w-[9rem]' src={img5} alt="" />
+            </div>
+
+            <div className='mt-20'>
+            <h1 className="fontTitle text-red-500 text-center">Online Partners</h1>
+                <img src={img6} alt="" />
             </div>
         </div>
     );
